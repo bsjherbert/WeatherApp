05193d505d30d05919bf4c51a00fcb8c6f017532
Steven <bsjherbert@gmail.com>
2019-11-14 20:12:41 -0500

    Changed Https

--- assets/weather.js
+++ assets/weather.js
@@ -5,15 +5,14 @@ $( document ).ready(function() {
     $("#submitButton").on("click", function(){
         let userInput = $("#inputField").val()
         $.ajax({
-            url: `http://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=fed4bfc9fcef4c8b786e332a5b1ff405`,
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=fed4bfc9fcef4c8b786e332a5b1ff405`,
             method: "GET"
         
             }).then(function (response) {
                 console.log(response)
 
                 $("#city").text(response.name);
-               
-                 $("#date").moment().format("MMM Do YY");
+                // $("#date").moment().format("MMM Do YY");
                 // $("#icon").html(response.weather[0].icon);
                 $("#temp").text("Temperature: " + response.main.temp);
                 $("#humid").text("Humidity: " + response.main.humidity);
